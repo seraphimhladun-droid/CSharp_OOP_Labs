13c2a8e70724b24fa7f5135da4718afadce7b6e1
Гладун Серафим <seraphimhladun@knu.ua>
2026-05-17 18:56:55 +0300

    Додано перевантаження унарних операторів

--- OOP_Labs/CourseProject_SmartManager/Task.cs
+++ OOP_Labs/CourseProject_SmartManager/Task.cs
@@ -4,52 +4,58 @@ namespace CourseProject_SmartManager
 {
     public class Task
     {
-        private string _title;
-
-        public string Title
-        {
-            get { return _title; }
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                    _title = "Безіменне завдання";
-                else
-                    _title = value;
-            }
-        }
-
+        public string Title { get; set; }
         public string Priority { get; set; }
-        public bool IsCompleted { get; private set; } // Нова властивість для статусу
-
-        static Task() { }
+        public bool IsCompleted { get; set; } 
 
         public Task(string title, string priority)
         {
             Title = title;
             Priority = priority;
-            IsCompleted = false; // За замовчуванням нове завдання не виконане
+            IsCompleted = false; // При створенні завдання завжди НЕ виконане
         }
 
-        public Task(Task previousTask)
-        {
-            this.Title = previousTask.Title + " (Копія)";
-            this.Priority = previousTask.Priority;
-            this.IsCompleted = false;
-        }
-
-        // ================= ВЕРСІЯ 3 =================
-
-        // Метод дії
         public void CompleteTask()
         {
             IsCompleted = true;
-            Console.WriteLine($"[Завдання]: Роботу над '{Title}' завершено!");
         }
 
-        // Предикатна функція
-        public bool IsDone()
+        public bool IsDone() // Предикатна функція
         {
             return IsCompleted;
+        }
+
+        //  Перевантаження унарних операторів =================
+
+        // 1. Оператор ++ (Інкремент) Працює як "штамп ВИКОНАНО"
+        public static Task operator ++(Task t)
+        {
+            t.IsCompleted = true;
+            return t; // Повертаємо оновлену картку завдання
+        }
+
+        // 2. Оператор -- (Декремент) Працює як "штамп СКАСОВАНО/ВЕРНУТИ В РОБОТУ"
+        public static Task operator --(Task t)
+        {
+            t.IsCompleted = false;
+            return t;
+        }
+
+        // 3. Оператор ! (Логічне НІ) 
+        // Якщо завдання виконане (true), поверне false. І навпаки.
+        public static bool operator !(Task t)
+        {
+            return !t.IsCompleted;
+        }
+
+        public static bool operator true(Task t)
+        {
+            return t.IsCompleted == true;
+        }
+
+        public static bool operator false(Task t)
+        {
+            return t.IsCompleted == false;
         }
     }
 }
